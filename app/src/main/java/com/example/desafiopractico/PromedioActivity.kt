c1d32e8b9150a0421e1e02d9a31704795d389c4e
Tiffany349 <br250073@alumno.udb.edu.sv>
2026-08-13 20:29:19 -0600

    feat: add return button navigation

--- app/src/main/java/com/example/desafiopractico/PromedioActivity.kt
+++ app/src/main/java/com/example/desafiopractico/PromedioActivity.kt
@@ -1,6 +1,8 @@
 package com.example.desafiopractico
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class PromedioActivity : AppCompatActivity() {
@@ -8,5 +10,13 @@ class PromedioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_promedio)
+
+        val btnRegresar = findViewById<Button>(R.id.btnRegresarPromedio)
+
+        btnRegresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
