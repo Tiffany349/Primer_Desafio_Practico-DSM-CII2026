a497a5c78a107918bee7be77df60120522686f9c
Tiffany349 <br250073@alumno.udb.edu.sv>
2026-08-13 19:57:11 -0600

    feat: average eyelash design

--- app/src/main/java/com/example/desafiopractico/MainActivity.kt
+++ app/src/main/java/com/example/desafiopractico/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.desafiopractico
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEjercicio1 = findViewById<Button>(R.id.btnEjercicio1)
+
+        btnEjercicio1.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/desafiopractico/PromedioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desafiopractico
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PromedioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promedio)
+    }
+}
